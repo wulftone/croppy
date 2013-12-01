90eaafa70d2c2d1a0cd67524309dd274f50c05b3
wulftone <enabfluw@gmail.com>
2013-11-30 18:26:12 -0800

    added more junk

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', 'mochaTest');
+
+  grunt.initConfig({
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          growl: true,
+          require: 'coffee-script'
+        },
+        src: ['test/**/*.js', 'test/**/*.coffee']
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['index.coffee', 'src/**/*.coffee', 'test/**/*.coffee'],
+        tasks: ['mochaTest', 'browserify'],
+      },
+      example: {
+        files: ['dist/*.js', 'example/*.html', 'Gruntfile.js'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          'dist/trickster.js': ['src/trickster.coffee'],
+        },
+        options: {
+          transform: ['coffeeify'],
+          standalone: 'Trickster'
+        }
+      }
+    }
+  });
+
+};
